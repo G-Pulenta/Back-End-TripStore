7b784623378526de169abab628896d5e840e0ded
Aldo Medina <u202018286@upc.edu.pe>
2023-06-08 11:53:34 -0500

    TripStore back-end project init

---
+++ src/main/java/com/upc/edu/BackEndTripStore/BackEndTripStoreApplication.java
@@ -0,0 +1,13 @@
+package com.upc.edu.BackEndTripStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTripStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTripStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/edu/BackEndTripStore/BackEndTripStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.edu.BackEndTripStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTripStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
