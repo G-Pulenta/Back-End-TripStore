5cdaaffb5f5a69f36c9e064c2828b261cb7cd9b6
DelfyNoLimits <nickhardav@gmail.com>
2023-06-24 20:35:39 -0500

    Test: Validation parameters work

--- src/test/java/com/upc/edu/BackEndTripStore/controller/UserControllerTest.java
+++ src/test/java/com/upc/edu/BackEndTripStore/controller/UserControllerTest.java
@@ -54,7 +54,7 @@ class UserControllerTest {
         assertTrue(actualMessage.contains(expectedMessage));
     }
     @Test
-    void testValidateCompleteInfo1() {
+    void testValidateNoUsername() {
         var user=new User();
         var userService= new UserService() {
             @Override
@@ -88,9 +88,154 @@ class UserControllerTest {
             }
         };
         UserController instancia = new UserController(userService);
-        user.setUsername("SDas");
-        user.setPassword("");
-        Exception exception = assertThrows(ValidationException.class, () -> {instancia.validateUser(user); });
+
+
+        user.setPassword("Haro");
+        user.setName("Nicolas");
+        user.setPhone("123456789");
+        user.setEmail("Nickhardav@gamil.com");
+        user.setLastname("Dávila");
+
+         assertThrows(ValidationException.class, () -> {instancia.validateUser(user); });
+
+    }
+    @Test
+    void testValidateNoPassword() {
+        var user=new User();
+        var userService= new UserService() {
+            @Override
+            public List<User> getAllUsers() {
+                return null;
+            }
+
+            @Override
+            public User getUserById(int id) {
+                return null;
+            }
+
+            @Override
+            public User createUser(User user) {
+                return null;
+            }
+
+            @Override
+            public User updateUser(int id, User updatedUser) {
+                return null;
+            }
+
+            @Override
+            public void deleteUser(int id) {
+
+            }
+
+            @Override
+            public User getUserByUsernameAndPassword(String username, String password) {
+                return null;
+            }
+        };
+        UserController instancia = new UserController(userService);
+
+        user.setUsername("Nicolas420");
+
+        user.setName("Nicolas");
+        user.setPhone("123456789");
+        user.setEmail("Nickhardav@gamil.com");
+        user.setLastname("Dávila");
+
+        assertThrows(ValidationException.class, () -> {instancia.validateUser(user); });
+
+    }
+
+    @Test
+    void testValidateNoEmail() {
+        var user=new User();
+        var userService= new UserService() {
+            @Override
+            public List<User> getAllUsers() {
+                return null;
+            }
+
+            @Override
+            public User getUserById(int id) {
+                return null;
+            }
+
+            @Override
+            public User createUser(User user) {
+                return null;
+            }
+
+            @Override
+            public User updateUser(int id, User updatedUser) {
+                return null;
+            }
+
+            @Override
+            public void deleteUser(int id) {
+
+            }
+
+            @Override
+            public User getUserByUsernameAndPassword(String username, String password) {
+                return null;
+            }
+        };
+        UserController instancia = new UserController(userService);
+
+        user.setUsername("Nicolas420");
+        user.setPassword("Haro");
+        user.setName("Nicolas");
+        user.setPhone("123456789");
+
+        user.setLastname("Dávila");
+
+        assertThrows(ValidationException.class, () -> {instancia.validateUser(user); });
+
+    }
+    @Test
+    void testValidateNoLastName() {
+        var user=new User();
+        var userService= new UserService() {
+            @Override
+            public List<User> getAllUsers() {
+                return null;
+            }
+
+            @Override
+            public User getUserById(int id) {
+                return null;
+            }
+
+            @Override
+            public User createUser(User user) {
+                return null;
+            }
+
+            @Override
+            public User updateUser(int id, User updatedUser) {
+                return null;
+            }
+
+            @Override
+            public void deleteUser(int id) {
+
+            }
+
+            @Override
+            public User getUserByUsernameAndPassword(String username, String password) {
+                return null;
+            }
+        };
+        UserController instancia = new UserController(userService);
+
+        user.setUsername("Nicolas420");
+        user.setPassword("Haro");
+        user.setName("Nicolas");
+        user.setPhone("123456789");
+        user.setEmail("Nickhardav@gamil.com");
+
+
+        assertThrows(ValidationException.class, () -> {instancia.validateUser(user); });
 
     }
 
