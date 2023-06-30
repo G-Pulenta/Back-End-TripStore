c385bdbf48e4c34ecba98082140d4b4ef0fe6cb7
Aldo Medina <u202018286@upc.edu.pe>
2023-06-30 13:38:46 -0500

    Feat: Order BDD featured..

---
+++ src/main/java/com/upc/edu/BackEndTripStore/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.upc.edu.BackEndTripStore.repository;
+
+import com.upc.edu.BackEndTripStore.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
